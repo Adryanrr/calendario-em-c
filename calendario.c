3c2e13b9e5d47584cd4a2f77770b8dc510465c12
Adryan Ryan <adryanryan.s.g@hotmail.com>
2024-05-04 13:55:13 -0300

    Centralizando o menu

--- calendario.c
+++ calendario.c
@@ -5,31 +5,31 @@
 #include <string.h>
 
 void MenuPrincipal(){
-    printf("+=================================================+\n");
-    printf("| 1- Criar Lembretes    | 2- Visualizar Lembretes |\n");
-    printf("+=================================================+\n");
-    printf("| 3- Analisar Lembretes | 4- Sair                 |\n");
-    printf("+=================================================+\n");
+    printf("+==================================================+\n");
+    printf("| 1 - Criar Lembretes    | 2 - Visualizar Lembretes |\n");
+    printf("+==================================================+\n");
+    printf("| 3 - Analisar Lembretes |        4 - Sair         |\n");
+    printf("+==================================================+\n");
 
 }
 void MenuCriar (){
-    printf("+=================================================+\n");
-    printf("| 1- Inserir Data       | 2- Inserir Hora         |\n");
-    printf("+=================================================+\n");
-    printf("| 3- Inserir Descrição  | 4- Menu Principal       |\n");
-    printf("+=================================================+\n");
+    printf("+==================================================+\n");
+    printf("|    1- Inserir Data    |     2- Inserir Hora      |\n");
+    printf("+==================================================+\n");
+    printf("| 3- Inserir Descrição  |    4- Menu Principal     |\n");
+    printf("+==================================================+\n");
 }
 void MenuVisualizar(){
     printf("+=================================================+\n");
-    printf("| 1- Semana              | 2- Mês                 |\n");
+    printf("|    1- Semana           |       2- Mês           |\n");
     printf("+=================================================+\n");
-    printf("| 3- Ano                 | 4- Menu Principal      |\n");
+    printf("| 3- Ano                 |    4- Menu Principal   |\n");
     printf("+=================================================+\n");
 }
 void MenuAnalisar(){
     printf("+=================================================+\n");
-    printf("| 1- Modificar Data      | 2- Modificar Hora      |\n");
+    printf("|    1- Modificar Data   |   2- Modificar Hora    |\n");
     printf("+=================================================+\n");
-    printf("| 3- Modificar Descrição | 4- Menu Principal      |\n");
+    printf("| 3- Modificar Descrição |    4- Menu Principal   |\n");
     printf("+=================================================+\n");
 }
