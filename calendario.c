5309110e4ceb56e6e895d53ac27fafc2e5621dc1
Adryan Ryan <adryanryan.s.g@hotmail.com>
2024-05-05 20:27:25 -0300

    criando void do menu visualizar lembretes

--- calendario.c
+++ calendario.c
@@ -6,7 +6,12 @@
 // Function prototypes
 void MenuPrincipal();
 void MenuCriarLembrete();
+// ------------------------------
 void MenuVisualizarLembretes();
+void VisualizarLembretesSemana();
+void VisualizarLembretesMes();
+void VisualizarLembretesAno();
+// ------------------------------
 void MenuAnalisarLembretes();
 
 int main(){
@@ -22,7 +27,7 @@ void MenuPrincipal(){
     printf("+==================================================+\n");
     printf("| 3 - Analisar Lembretes |        4 - Sair         |\n");
     printf("+==================================================+\n");
-    printf("Digite Qual opção você deseja acessa: \n");
+    printf("Digite Qual opção você deseja acessar: \n");
     scanf("%d", &opMenuPrincipal);
 
     switch (opMenuPrincipal){
@@ -34,11 +39,6 @@ void MenuPrincipal(){
         break;
     case 3:
         MenuAnalisarLembretes();
-        break;
-    case 4:
-        printf("Deseja sair do calendário? \n");
-        break;
-    default:
         MenuPrincipal();
         break;
     }
@@ -60,17 +60,20 @@ void MenuVisualizarLembretes(){
     printf("+========================================================+\n");
     printf("|           3 - Ano            |    4 - Menu Principal   |\n");
     printf("+========================================================+\n");
+    printf("Digite Qual opção você deseja acessar: \n");
     scanf("%d", &opMenuVisualizarLembretes);
     switch (opMenuVisualizarLembretes){
     case 1:
+        VisualizarLembretesSemana();
         break;
     case 2:
+        VisualizarLembretesMes();
         break;
     case 3:
-        break;
-    case 4:
+        VisualizarLembretesAno();
         break;
     default:
+        MenuPrincipal();
         break;
     }
 }
@@ -84,4 +87,14 @@ void MenuAnalisarLembretes(){
     printf("| 3- Modificar Descrição |    4- Menu Principal   |\n");
     printf("+=================================================+\n");
     scanf("%d", &opMenuAnalisar);
+}
+
+void VisualizarLembretesSemana(){
+    printf("A função Visualizar lembretes por semana está em criação \n");
+}
+void VisualizarLembretesMes(){
+    printf("A função Visualizar lembretes por semana está em criação \n");
+}
+void VisualizarLembretesAno(){
+    printf("A função Visualizar lembretes por ano está em criação \n");
 }
