09136d3ec9f04139edc30ace1ab0238ead85dd76
Adryan Ryan <adryanryan.s.g@hotmail.com>
2024-05-05 01:48:17 -0300

    Corrigindo erros

---
+++ anotações.md
@@ -0,0 +1,41 @@
+## Menu Principal
+
+- **1** - Criar lembrete
+- **2** - Visualizar Calendário
+- **3** - Analisar lembretes
+  - **3.1** - Lista de lembretes por data
+- **4** - Sair
+
+---
+
+### Criar lembretes
+
+- Data
+- Horário
+- Descrição
+
+---
+
+### Visualizar o calendário
+
+1. Por semana
+2. Por ano
+3. Por mês
+4. Voltar
+
+#### Por Semana
+
+| Semana    |
+| ---------|
+| Segunda  |
+| Terça    |
+| Quarta   |
+| Quinta   |
+| Sexta    |
+| Sábado   |
+
+#### Por Ano
+
+- De hoje: Mostra todos os compromissos.
+- Até o mesmo dia no outro ano
+
--- calendario.c
+++ calendario.c
@@ -1,83 +1,87 @@
-//calendario
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-int opMenuPrincipal,opMenuCriar,opMenuVisualizar,opMenuAnalisar;
 
-<<<<<<< HEAD
-int opMenuPrincipal, opMenuCriar, opMenuVisualizar, opMenuAnalisar;
+// Function prototypes
+void MenuPrincipal();
+void MenuCriarLembrete();
+void MenuVisualizarLembretes();
+void MenuAnalisarLembretes();
+
+int main(){
+    MenuPrincipal(); 
+    return 0;
+}
 
 void MenuPrincipal(){
-    do{
+    int opMenuPrincipal;
+
     printf("+==================================================+\n");
     printf("| 1 - Criar Lembretes    | 2 - Visualizar Lembretes |\n");
     printf("+==================================================+\n");
     printf("| 3 - Analisar Lembretes |        4 - Sair         |\n");
     printf("+==================================================+\n");
-    printf("Digite Qual opção voce deseja acessar \n");
+    printf("Digite Qual opção você deseja acessa: \n");
     scanf("%d", &opMenuPrincipal);
+
     switch (opMenuPrincipal){
     case 1:
-        system("cls");
         MenuCriarLembrete();
         break;
     case 2:
-        system("cls");
         MenuVisualizarLembretes();
-    case 3: 
-        system ("cls");
+        break;
+    case 3:
         MenuAnalisarLembretes();
-    case 4: 
-        system ("cls");
-        printf ("Deseja sair do calendario? \n");
-    }    
- } while (MenuPrincipal != 4);
+        break;
+    case 4:
+        printf("Deseja sair do calendário? \n");
+        break;
+    default:
+        MenuPrincipal();
+        break;
+    }
 }
-void MenuCriarLembrete (){
+
+void MenuCriarLembrete(){
     printf("+====================================================+\n");
     printf("|    1 - Inserir Data    |     2 - Inserir Hora      |\n");
     printf("+====================================================+\n");
     printf("| 3 - Inserir Descrição  |    4 - Menu Principal     |\n");
     printf("+====================================================+\n");
 }
+
 void MenuVisualizarLembretes(){
+    int opMenuVisualizarLembretes;
+
     printf("+========================================================+\n");
     printf("|          1 - Semana          |         2 - Mês         |\n");
     printf("+========================================================+\n");
     printf("|           3 - Ano            |    4 - Menu Principal   |\n");
     printf("+========================================================+\n");
-=======
-void MenuPrincipal(){
-    printf("+=================================================+\n");
-    printf("| 1- Criar Lembretes    | 2- Visualizar Lembretes |\n");
-    printf("+=================================================+\n");
-    printf("| 3- Analisar Lembretes | 4- Sair                 |\n");
-    printf("+=================================================+\n");
-    scanf("%d",&opMenuPrincipal);
+    scanf("%d", &opMenuVisualizarLembretes);
+    switch (opMenuVisualizarLembretes){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    default:
+        break;
+    }
 }
-void MenuCriar (){
-    printf("+=================================================+\n");
-    printf("| 1- Inserir Data       | 2- Inserir Hora         |\n");
-    printf("+=================================================+\n");
-    printf("| 3- Inserir Descrição  | 4- Menu Principal       |\n");
-    printf("+=================================================+\n");
-    scanf("%d",&opMenuCriar);
-}
-void MenuVisualizar(){
-    printf("+=================================================+\n");
-    printf("| 1- Semana              | 2- Mês                 |\n");
-    printf("+=================================================+\n");
-    printf("| 3- Ano                 | 4- Menu Principal      |\n");
-    printf("+=================================================+\n");
-    scanf("%d",&opMenuVisualizar);
->>>>>>> 2285c9574772d04f7211c73c9c8f27fdc3d14fca
-}
+
 void MenuAnalisarLembretes(){
+    int opMenuAnalisar;
+
     printf("+=================================================+\n");
     printf("|    1- Modificar Data   |   2- Modificar Hora    |\n");
     printf("+=================================================+\n");
     printf("| 3- Modificar Descrição |    4- Menu Principal   |\n");
     printf("+=================================================+\n");
-    scanf("%d",&opMenuAnalisar);
+    scanf("%d", &opMenuAnalisar);
 }
