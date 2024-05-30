0bf85bb80725561eccaec997c071ae36b28dfe35
Matheus Jucá <matheusrjuca@gmail.com>
2024-05-30 10:57:25 -0300

    ajeitando o set locale pra mim

--- Code/Calendario.c
+++ Code/Calendario.c
@@ -12,35 +12,34 @@
 #define MAXIMO_LEMBRETES 10
 
 struct lembrete{
-    char nome[100];
+    char nome[50];
+    char descricao[250];
     int ano;
     int mes;       
-    int dia;          
+    int dia; 
 };
 struct lembrete lembretes[MAXIMO_LEMBRETES];
 
 
-// Funções de interface do usuário
+// Fun��es de interface do usu�rio
 void limparTerminal();
 void menuPrincipal();
 void logoCalendario();
-char obterEscolha();
 
-// Função de lembretes
+// Fun��o de lembretes
 // ---------------------
 
 
 // ---------------------
 
-// Funções de manipulação de datas
-int eAnoBissexto(int ano);
-int calcularPrimeiroDiaSemana(int ano, int mes);
-void imprimirCalendario(int ano, int mes, int dia, int mesArray[]);
-int intervalo1(const int minimo, const int maximo);
-int intervalo2(const int minimo, const int maximo);
-void obterDigitos(int num, char* primeiroDigito, char* segundoDigito);
+// Fun��es de manipula��o de datas
 
-// Funções específicas do calendário
+
+
+
+
+
+// Fun��es espec�ficas do calend�rio
 void visualizarLembretes();
 void calendarioIcev();
 void calendario();
@@ -61,17 +60,16 @@ void limparTerminal() {
     system("clear");
 #endif
 }
-
 void logoCalendario(){
 
-printf(" ██████╗ █████╗ ██╗     ███████╗███╗   ██╗██████╗  █████╗ ██████╗ ██╗ ██████╗  \n");
-printf("██╔════╝██╔══██╗██║     ██╔════╝████╗  ██║██╔══██╗██╔══██╗██╔══██╗██║██╔═══██╗ \n");
-printf("██║     ███████║██║     █████╗  ██╔██╗ ██║██║  ██║███████║██████╔╝██║██║   ██║ \n");
-printf("██║     ██╔══██║██║     ██╔══╝  ██║╚██╗██║██║  ██║██╔══██║██╔══██╗██║██║   ██║ \n");
-printf("╚██████╗██║  ██║███████╗███████╗██║ ╚████║██████╔╝██║  ██║██║  ██║██║╚██████╔╝ \n");
-printf(" ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝  \n");
-                                                                              
+printf(" ??????? ?????? ???     ????????????   ??????????  ?????? ??????? ??? ???????  \n");
+printf("???????????????????     ?????????????  ??????????????????????????????????????? \n");
+printf("???     ???????????     ??????  ?????? ??????  ?????????????????????????   ??? \n");
+printf("???     ???????????     ??????  ?????????????  ?????????????????????????   ??? \n");
+printf("???????????  ?????????????????????? ?????????????????  ??????  ??????????????? \n");
+printf(" ??????????  ??????????????????????  ???????????? ???  ??????  ?????? ???????  \n");
 }
+                                                                              
 
 void menuPrincipal(){
     setlocale(LC_ALL,"Portuguese_Brazil");
@@ -85,7 +83,7 @@ void menuPrincipal(){
     printf("|                                                                   |\n");
     printf("|                           0 - Exit                                |\n");
     printf("+===================================================================+\n");
-    printf("Digite Qual opção você deseja acessar: \n");
+    printf("Digite Qual op��o voc� deseja acessar: \n");
     scanf("%d", &opMenuPrincipal);
 
     switch (opMenuPrincipal){
@@ -110,23 +108,23 @@ void menuPrincipal(){
         printf("Obrigado, volte sempre! \n");
         break;
       default:
-        printf("Opção inválida, tente novamente! \n");
+        printf("Op��o inv�lida, tente novamente! \n");
         sleep(2);
         logoCalendario();
         limparTerminal();
         menuPrincipal();
       }
 }
-// Inicio da gestão de lembretes
+// Inicio da gest�o de lembretes
 
 
 void criarLembretes(){
     struct lembrete lembretes[100];
     printf("Digite o nome do lembrete: \n");
-    scanf("%s", lembretes[0].nome);
+    scanf("%s", &lembretes[0].nome);
     printf("Digite o ano do lembrete: \n");
     scanf("%d", &lembretes[0].ano);
-    printf("Digite o mês do lembrete: \n");
+    printf("Digite o m�s do lembrete: \n");
     scanf("%d", &lembretes[0].mes);
     printf("Digite o dia do lembrete: \n");
     scanf("%d", &lembretes[0].dia);
@@ -139,11 +137,11 @@ void numeroLembretes(struct lembrete lembretes[]){
     int i;
     int numLembretes = 0;
     for(i = 0; i < MAXIMO_LEMBRETES; i++){
-        if(lembretes[i].dia != 0){ // assumindo que 'dia' é um campo em 'struct lembrete' e que '0' indica um lembrete vazio
+        if(lembretes[i].dia != 0){ // assumindo que 'dia' � um campo em 'struct lembrete' e que '0' indica um lembrete vazio
             numLembretes++;
         }
     }
-    printf("numero de lembres é: %d \n", numLembretes);
+    printf("numero de lembres �: %d \n", numLembretes);
 }    
 
 
@@ -160,7 +158,7 @@ void visualizarLembretes(){
         printf("Pressione 0 para voltar ao menu principal: \n");    
         break;
     case 2:
-        printf("em construção \n");
+        printf("em constru��o \n");
         break;
     default:
         break;
@@ -169,53 +167,50 @@ void visualizarLembretes(){
 }
 
 void menuLembretes() {
-    printf("Pressione 1: criar lembretes \n"); // direcionar para a função de criar lembrete
+    printf("Pressione 1: criar lembretes \n"); // direcionar para a fun��o de criar lembrete
     printf("Pressione 2: visualizar lembretes \n");
     printf("Pressione 3: para voltar \n");
-    // função principal para o calendário
+    // fun��o principal para o calend�rio
 }
 
-// Final da gestão de lembretes
+// Final da gest�o de lembretes
 
-// inicio da gestão de calendario
+// inicio da gest�o de calendario
 
 static const char *MonthDisplay[] = {
-    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
+    "Janeiro", "Fevereiro", "Mar�o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
     "Outubro", "Novembro", "Dezembro"};
 int year, mes, dia, x, day2;
-signed char c; // entrada do usuário para mudança de calendário
+signed char c; // entrada do usu�rio para mudan�a de calend�rio
 int mesArray[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 void menuCalendario() {
-    printf("Você selecionou %s %d, %d \n", MonthDisplay[mes - 1], dia, year);
-    printf("   - Para alternar entre os dias, use (a) para o dia anterior e (d) para o próximo dia. \n");
-    printf("   - Para alternar entre os meses, use (w) para o mês anterior e (s) para o próximo mês.\n\n");
+    printf("Voc� selecionou %s %d, %d \n", MonthDisplay[mes - 1], dia, year);
+    printf("   - Para alternar entre os dias, use (a) para o dia anterior e (d) para o pr�ximo dia. \n");
+    printf("   - Para alternar entre os meses, use (w) para o m�s anterior e (s) para o pr�ximo m�s.\n\n");
 
-    printf("Pressione 1: criar lembretes \n"); // direcionar para a função de criar lembrete
-    printf("Pressione 2: visualizar lembretes \n");
-    printf("Pressione 3: para voltar \n");
-    // função principal para o calendário
+    menuLembretes();
 }
 
 void GerarCalendario() {
     // Fonte da linha acima: cadaeit.net
-    system("setterm -bold on");                                    // cabeçalho em negrito
-    printf("\n         %s    %d \n", MonthDisplay[mes - 1], year); // cabeçalho
+    system("setterm -bold on");                                    // cabe�alho em negrito
+    printf("\n         %s    %d \n", MonthDisplay[mes - 1], year); // cabe�alho
     printf("\nDOM  SEG  TER  QUA  QUI  SEX  SAB\n");
     system("setterm -bold off");
 
-    for (day2 = 1; day2 <= x; day2++) { // day2 é o contador
+    for (day2 = 1; day2 <= x; day2++) { // day2 � o contador
         printf("     ");
-    } // inicializa a posição do primeiro dia
+    } // inicializa a posi��o do primeiro dia
 
     for (day2 = 1; day2 <= mesArray[mes - 1]; day2++) {
-        // preenche o calendário
-        if (dia == day2) { // torna a saída vermelha
-            printf("\e[5;31;40m%2d\e[0m", dia); // não pisca
+        // preenche o calend�rio
+        if (dia == day2) { // torna a sa�da vermelha
+            printf("\e[5;31;40m%2d\e[0m", dia); // n�o pisca
         } else {
             printf("%2d", day2);
         }
-        if (((day2 + x) % 7) > 0) { // passa para a próxima linha após sábado
+        if (((day2 + x) % 7) > 0) { // passa para a pr�xima linha ap�s s�bado
             printf("   ");
         } else {
             printf("\n ");
@@ -226,57 +221,57 @@ void GerarCalendario() {
 
 void calendario() {
     limparTerminal();
-    system("setterm -bold on"); // cabeçalho em negrito
-    printf("Calendário de Eventos \n");
+    system("setterm -bold on"); // cabe�alho em negrito
+    printf("Calend�rio de Eventos \n");
     system("setterm -bold off");
 
     printf("Digite o ano: ");
-    scanf("%d", &year); // entrada do usuário para o ano
+    scanf("%d", &year); // entrada do usu�rio para o ano
     while (year < 0) {
-        printf("Ano não adequado. \n");
+        printf("Ano n�o adequado. \n");
         printf("Digite o ano: ");
-        scanf("%d", &year); // entrada do usuário para o ano
+        scanf("%d", &year); // entrada do usu�rio para o ano
     }
 
-    printf("Digite o mês: ");
-    scanf("%d", &mes); // entrada do usuário para o mês
+    printf("Digite o m�s: ");
+    scanf("%d", &mes); // entrada do usu�rio para o m�s
     while (mes < 1 || mes > 12) {
-        printf("Mês não adequado. \n");
-        printf("Digite o mês: ");
-        scanf("%d", &mes); // entrada do usuário para o mês
+        printf("M�s n�o adequado. \n");
+        printf("Digite o m�s: ");
+        scanf("%d", &mes); // entrada do usu�rio para o m�s
     }
 
     printf("Digite o dia: ");
-    scanf("%d", &dia); // entrada do usuário para o dia
+    scanf("%d", &dia); // entrada do usu�rio para o dia
     while (dia < 1 || dia > 31) {
-        printf("Dia não adequado. \n");
+        printf("Dia n�o adequado. \n");
         printf("Digite o dia: ");
-        scanf("%d", &dia); // entrada do usuário para o dia
+        scanf("%d", &dia); // entrada do usu�rio para o dia
     }
 
-    // Verifica validade do dia para o mês
+    // Verifica validade do dia para o m�s
     while (true) {
         if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
             if (dia > 30) {
-                printf("Dia não adequado. \n");
+                printf("Dia n�o adequado. \n");
                 printf("Digite o dia: ");
-                scanf("%d", &dia); // entrada do usuário para o dia
+                scanf("%d", &dia); // entrada do usu�rio para o dia
                 continue;
             }
         } else if (mes == 2) {
             if (((year % 4) == 0 && (year % 100 != 0)) || (year % 400 == 0)) {
                 mesArray[1] = 29;
                 if (dia > 29) {
-                    printf("Dia não adequado. \n");
+                    printf("Dia n�o adequado. \n");
                     printf("Digite o dia: ");
-                    scanf("%d", &dia); // entrada do usuário para o dia
+                    scanf("%d", &dia); // entrada do usu�rio para o dia
                     continue;
                 }
             } else {
                 if (dia > 28) {
-                    printf("Dia não adequado. \n");
+                    printf("Dia n�o adequado. \n");
                     printf("Digite o dia: ");
-                    scanf("%d", &dia); // entrada do usuário para o dia
+                    scanf("%d", &dia); // entrada do usu�rio para o dia
                     continue;
                 }
             }
@@ -284,7 +279,7 @@ void calendario() {
         break;
     }
 
-    // Calcula o primeiro dia do mês
+    // Calcula o primeiro dia do m�s
     x = 1;
     x = (x += mes < 3 ? year : year - 2, 23 * mes / 9 + x + 4 + year / 4 - year / 100 + year / 400) % 7;
     if ((year % 4) == 0 && ((year % 100) != 0 || (year % 400) == 0)) {
@@ -299,29 +294,29 @@ void calendario() {
         mesArray[1] = 28;
     }
 
-    // Imprimindo calendário
+    // Imprimindo calend�rio
     limparTerminal();
     GerarCalendario();
     menuCalendario();
     
     while (true) {
-        scanf(" %c", &c); // leia o comando do usuário
+        scanf(" %c", &c); // leia o comando do usu�rio
 
         if (c == '1') {
-            // Chamar função para criar evento
-            printf("Função de criar evento não implementada.\n");
+            // Chamar fun��o para criar evento
+            printf("Fun��o de criar evento n�o implementada.\n");
             GerarCalendario();
             menuCalendario();
         } else if (c == '2') {
-            // Chamar função para deletar evento
-            printf("Função de deletar evento não implementada.\n");
+            // Chamar fun��o para deletar evento
+            printf("Fun��o de deletar evento n�o implementada.\n");
             limparTerminal();
             GerarCalendario();
         } else if (c == '3') {
             limparTerminal();
             menuPrincipal();
             break;
-        } else if (c == 'w') { // mês anterior
+        } else if (c == 'w') { // m�s anterior
             if (mes == 1) {
                 year--;
                 mes = 12;
@@ -334,7 +329,7 @@ void calendario() {
             limparTerminal();
             GerarCalendario();
             menuCalendario();
-        } else if (c == 's') { // próximo mês
+        } else if (c == 's') { // pr�ximo m�s
             if (mes == 12) {
                 mes = 1;
                 year++;
@@ -362,7 +357,7 @@ void calendario() {
             limparTerminal();
             GerarCalendario();
             menuCalendario();
-        } else if (c == 'd') { // próximo dia
+        } else if (c == 'd') { // pr�ximo dia
             if (dia == mesArray[mes - 1]) {
                 dia = 1;
                 if (mes == 12) {
@@ -378,7 +373,7 @@ void calendario() {
             GerarCalendario();
             menuCalendario();
         } else {
-            printf("Resposta inválida. Por favor, tente novamente. \n");
+            printf("Resposta inv�lida. Por favor, tente novamente. \n");
             limparTerminal();
             GerarCalendario();
             menuCalendario();
@@ -387,21 +382,21 @@ void calendario() {
 
 }
 
-// Final da gestão de calendario
+// Final da gest�o de calendario
 
 void calendarioIcev(){
     int operecao;
 
     printf("+=======|==========================================================================+\n");
-    printf("|HORARI0|    SEGUNDA    |    TERÇA    |    QUARTA    |    QUINTA     |    SEXTA    |\n");
+    printf("|HORARI0|    SEGUNDA    |    TER�A    |    QUARTA    |    QUINTA     |    SEXTA    |\n");
     printf("|-------|---------------|-------------|--------------|---------------|-------------|\n");
     printf("| 14:30 |  Arquiterura  |  Matematica | Arquiterura  |  Algoritimo   | Algoritimo  |\n");
     printf("|       |       de      |   Discreta  |     de       |       e       |      e      |\n");
-    printf("| 16:10 |  computadores |             | computadores |  Programação  | Programação |\n");
+    printf("| 16:10 |  computadores |             | computadores |  Programa��o  | Programa��o |\n");
     printf("|-------|---------------|-------------|--------------|---------------|-------------|\n");
     printf("| 16:20 |               |  Engenharia |  Matematica  |   Engenharia  |  Projeto    |\n");
     printf("|       | Seminarios I  |      de     |   Discreta   |       de      |     de      |\n");
-    printf("| 18:00 |               |   Software  |              |    Software   | Extensão I  |\n");
+    printf("| 18:00 |               |   Software  |              |    Software   | Extens�o I  |\n");
     printf("|-------|---------------|-------------|--------------|---------------|-------------|\n");
     printf("| 18:10 |               |             |              |               |             |\n");
     printf("|       |   Ingles I    |             |              |               |             |\n");
@@ -416,7 +411,7 @@ void calendarioIcev(){
         menuPrincipal();
         break;  
     default:
-        printf("opção invalida! \n");
+        printf("op��o invalida! \n");
         break;
     }
 }
