f9448326d3f50f5eb601ea324f64ff0d8a2196bc
Adryanrr <adryanryan.s.g@hotmail.com>
2024-05-31 15:51:54 -0300

    Cadastro de usuarios

--- Code/arquivos/Backup estruturas/cadastroUsuario.c
+++ Code/arquivos/Backup estruturas/cadastroUsuario.c
@@ -5,10 +5,19 @@
 
 void MenuPrincipal();
 
+FILE *arquivo;
 // void cadastrarUsuario(struct usuario usuarios[], int *contagem);  
 // void listarUsuarios(int *contagem, struct usuario usuarios[]);
+void criarArquivoUser(){
+  arquivo = fopen("usuarios.txt", "a");
+  if (arquivo == NULL){
+    printf("Erro ao criar arquivo! \n");
+    exit(1);
+  }
+  fclose(arquivo);
+}
 
-#define MAXIMO_USUARIO 3
+#define MAXIMO_USUARIO 10
 
 // void limparTerminal() {
 // #ifdef _WIN32
@@ -26,23 +35,45 @@ struct usuario{
   int contagem = 0;
 
 void cadastrarUsuario(struct usuario usuarios[], int *contagem){
+
+  arquivo = fopen("usuarios.txt", "a");
   if (*contagem >= MAXIMO_USUARIO){
     printf("Numero maximo de usuarios atingidos! \n");
     return;
   }
+
   struct usuario novoUsuario;
   printf("Insira o nome de usuario: \n");
   scanf("%s", novoUsuario.nomeUsuario);
+  if(compararUsuario(novoUsuario.nomeUsuario, contagem)==1){
+    printf("já existe\n");
+  }
+
   printf("Insira a senha do usuario: \n");
   scanf("%s", novoUsuario.senha);
+  fprintf(arquivo, "%s" , novoUsuario.nomeUsuario);
+  
   usuarios[*contagem] = novoUsuario;
   (*contagem)++;
 
-  printf("Usuario cadastrado com sucesso!");
+  printf("Usuario cadastrado com sucesso! \n");
+
+  fclose(arquivo);
 
   MenuPrincipal();
 }
 
+int compararUsuario(char usuario, int *contagem){
+  struct usuario novoUsuario;
+  
+  for (int i = 0; i < *contagem; i++){
+    if (strcmp(usuario, usuarios[i].nomeUsuario) == 0){
+      printf("Usuario encontrado! \n");
+      printf("Tente outro nome de usuario! \n");
+      return 1;
+    }
+  }
+}
 void listarUsuarios(int *contagem, struct usuario usuarios[]){
   int i;
   for (i = 0; i < 2; i++){
@@ -54,6 +85,7 @@ void listarUsuarios(int *contagem, struct usuario usuarios[]){
 }
 
 int main(){
+
   MenuPrincipal();
 }
 
